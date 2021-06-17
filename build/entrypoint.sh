93c0211ccf825f902a6d97b2f10441446c62613a
airenas <airenass@gmail.com>
2021-06-17 16:23:44 +0300

    Fix docker build scripts

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+set -e
 echo Starting
 sed -i "s|<base href=\"/music\">|<base href=\"$BASE_HREF\">|" /usr/share/nginx/html/index.html
 echo Updated base ref to $BASE_HREF
 echo Starting nginx
-nginx -g 'daemon off;'
+exec nginx -g 'daemon off;'
