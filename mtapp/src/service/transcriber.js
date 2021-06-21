b67914ba158b713d8adb31647f15d99bd11fde2d
airenas <airenass@gmail.com>
2021-06-21 19:06:53 +0300

    Drop logs

--- mtapp/src/service/transcriber.js
+++ mtapp/src/service/transcriber.js
@@ -5,11 +5,9 @@ export default class Transcriber {
   
   static initURL = (url) => {
     this.url = url;  
-    console.log('transcriber url', this.url)
   }
   
   transcribe = (file, instrument) => {
-    console.log('transcriber url', Transcriber.url)
     const data = new FormData();
     data.append('instrument', instrument);
     data.append('file', file);
